c09d3bd057aa42bd622f4b96c9e7fba591c0362d
Alvaro Fresquet <29437693+afresquet@users.noreply.github.com>
2023-12-04 22:20:17 +0100

    day 3 examples pass but puzzle answers are wrong :(

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,4 +1,9 @@
-use nom::{bytes::complete::take_till, character::complete::digit0, IResult};
+use std::{collections::BTreeMap, ops::RangeInclusive};
+
+use nom::{
+    bytes::complete::take_till, character::complete::digit1, multi::many0, sequence::preceded,
+    IResult,
+};
 
 advent_of_code::solution!(3);
 
@@ -9,14 +14,20 @@ struct PartNumber<'a> {
 }
 
 impl PartNumber<'_> {
-    pub fn is_valid(&self, grid: &[Vec<char>]) -> bool {
-        let row_indeces =
-            (self.position.0.checked_sub(1).unwrap_or(self.position.0))..=(self.position.0 + 1);
-        let column_indeces = (self.position.1.checked_sub(1).unwrap_or(self.position.1))
-            ..=(self.position.1 + self.number.len());
+    fn ranges(&self) -> (RangeInclusive<usize>, RangeInclusive<usize>) {
+        let start_row = self.position.0.checked_sub(1).unwrap_or(self.position.0);
+        let end_row = self.position.0 + 1;
+        let start_column = self.position.1.checked_sub(1).unwrap_or(self.position.1);
+        let end_column = self.position.1 + self.number.len();
 
-        for row in row_indeces {
-            for column in column_indeces.clone() {
+        (start_row..=end_row, start_column..=end_column)
+    }
+
+    pub fn is_valid(&self, grid: &[Vec<char>]) -> bool {
+        let (row_range, column_range) = self.ranges();
+
+        for row in row_range {
+            for column in column_range.clone() {
                 if let Some(symbol) = grid.get(row).and_then(|row| row.get(column)) {
                     if !symbol.is_ascii_digit() && *symbol != '.' {
                         return true;
@@ -27,21 +38,28 @@ impl PartNumber<'_> {
 
         false
     }
+
+    pub fn is_touching_gear(&self, grid: &[Vec<char>]) -> Option<(usize, usize)> {
+        let (row_range, column_range) = self.ranges();
+
+        for row in row_range {
+            for column in column_range.clone() {
+                if let Some(symbol) = grid.get(row).and_then(|row| row.get(column)) {
+                    if *symbol == '*' {
+                        return Some((row, column));
+                    }
+                }
+            }
+        }
+
+        None
+    }
 }
 
 fn parse_line_numbers(input: &str) -> IResult<&str, Vec<&str>> {
-    let mut string = input;
-    let mut numbers = vec![];
-    while !string.is_empty() {
-        let (input, _) = take_till(|c: char| c.is_ascii_digit())(string)?;
-        let (input, number) = digit0(input)?;
-        if !number.is_empty() {
-            numbers.push(number);
-        }
-        string = input;
-    }
+    let (input, numbers) = many0(preceded(take_till(|c: char| c.is_ascii_digit()), digit1))(input)?;
 
-    Ok((string, numbers))
+    Ok((input, numbers))
 }
 
 fn parse_numbers(input: &str) -> Vec<PartNumber> {
@@ -63,11 +81,15 @@ fn parse_numbers(input: &str) -> Vec<PartNumber> {
     numbers.collect()
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let grid = input
+fn parse_grid(input: &str) -> Vec<Vec<char>> {
+    input
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let grid = parse_grid(input);
     let numbers = parse_numbers(input);
 
     let part_numbers = numbers.iter().filter_map(|number| {
@@ -80,65 +102,31 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = parse_grid(input);
+    let numbers = parse_numbers(input);
+    let mut gears: BTreeMap<(usize, usize), Vec<u32>> = BTreeMap::new();
+
+    for number in numbers {
+        if let Some(coordinates) = number.is_touching_gear(&grid) {
+            let n = number.number.parse::<u32>().unwrap();
+            gears
+                .entry(coordinates)
+                .and_modify(|v| v.push(n))
+                .or_insert_with(|| vec![n]);
+        }
+    }
+
+    let gear_ratios = gears
+        .values()
+        .filter(|v| v.len() == 2)
+        .map(|v| v.iter().product::<u32>());
+
+    Some(gear_ratios.sum())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn part_number_side() {
-        let result = part_one(
-            ".....
-.123.
-.._..",
-        );
-        assert_eq!(result, Some(123));
-    }
-
-    #[test]
-    fn part_number_diagonal() {
-        let result = part_one(
-            ".....
-.123.
-...._",
-        );
-        assert_eq!(result, Some(123));
-    }
-
-    #[test]
-    fn part_number_small_grid() {
-        let result = part_one("123_");
-        assert_eq!(result, Some(123));
-    }
-
-    #[test]
-    fn no_part_number() {
-        let result = part_one("123");
-        assert_eq!(result, Some(0));
-    }
-
-    #[test]
-    fn single_part_number() {
-        let result = part_one(
-            ".........
-.123.321.
-..._.....",
-        );
-        assert_eq!(result, Some(123));
-    }
-
-    #[test]
-    fn double_part_number() {
-        let result = part_one(
-            ".........
-.123.321.
-...._....",
-        );
-        assert_eq!(result, Some(444));
-    }
-
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
@@ -146,16 +134,20 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "pending"]
     fn input_part_one() {
         let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
         assert_eq!(result, Some(540131));
     }
 
     #[test]
-    #[ignore = "pending"]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(467835));
+    }
+
+    #[test]
+    fn input_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(86879020));
     }
 }
